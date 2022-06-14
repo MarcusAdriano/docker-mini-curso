a9f5b336b895d7ffc5ae3c64b6a5c2969b830bfc
Marcus Adriano <marcusadriano.pereira@gmail.com>
2022-06-14 19:07:54 -0300

    alteracao no CORS

--- src/main/java/io/github/marcusadriano/todo/TodoController.java
+++ src/main/java/io/github/marcusadriano/todo/TodoController.java
@@ -6,21 +6,25 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = { RequestMethod.GET, RequestMethod.POST,
+		RequestMethod.DELETE })
 @Controller("/api/v1")
 @RestController
 public class TodoController {
-    
-    @Autowired
+
+	@Autowired
 	private TodoRepository repo;
 
-    @GetMapping("/")
+	@GetMapping("/")
 	public ResponseEntity<Collection<TodoEntity>> findAll() {
 		return ResponseEntity.ok(repo.findAll());
 	}
@@ -31,9 +35,9 @@ public class TodoController {
 		return ResponseEntity.ok(result);
 	}
 
-    @DeleteMapping("/{id}")
-    public void deleteById(@PathVariable("id") Integer id) {
-        repo.deleteAllById(Arrays.asList(id));
-    }
+	@DeleteMapping("/{id}")
+	public void deleteById(@PathVariable("id") Integer id) {
+		repo.deleteAllById(Arrays.asList(id));
+	}
 
 }
